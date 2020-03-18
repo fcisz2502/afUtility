ae0a0ba6d7e282dbd10ba48ada5ccb17baf17a18
fcisz2502 <info@fcisz.com>
2020-03-18 23:45:02 +0800

    add orderReviewCollector

--- orderReviewCollector.py
+++ orderReviewCollector.py
@@ -13,23 +13,22 @@ def getAllOrderReview():
     
     orderReviewTotalResult = pd.DataFrame()
     for doc in os.listdir(reviewResultFolder):
-        if today in doc.split("_"):
-            checkList.remove("_".join(doc.split("_")[:2]))
-            try:
-                orderReviewTotalResult = pd.concat([orderReviewTotalResult, pd.read_csv(os.path.join(reviewResultFolder, doc))], sort=False)
-            except TypeError:
-                orderReviewTotalResult = pd.concat([orderReviewTotalResult, pd.read_csv(os.path.join(reviewResultFolder, doc))])
+        if today in doc.split("_") and (not "allSpikeOrderReview.csv" in doc.split("_")):
+            checkList.remove(doc[18:-4])
+            orderReviewTotalResult = pd.concat([orderReviewTotalResult, pd.read_csv(os.path.join(reviewResultFolder, doc))], sort=False)
+#            except TypeError:
+#                orderReviewTotalResult = pd.concat([orderReviewTotalResult, pd.read_csv(os.path.join(reviewResultFolder, doc))])
     if len(checkList):
         content = "Order review result(s) from %s have not been found!"%checkList
     else:
         content = "Order review for %s have been done!"%["VM2_spike2", "VM2_spikeOLOS", "VM4_spike2", "VM5_spike2"]
     
-    allSpikeOrderReview = os.path.join(reviewResultFolder, "allSpikeOrderReview.csv")
+    allSpikeOrderReview = os.path.join(reviewResultFolder, datetime.now().strftime("%Y-%m-%d_%H%M%S")+"_allSpikeOrderReview.csv")
     orderReviewTotalResult.to_csv(allSpikeOrderReview, index=0)
     
-    email = Email()
-    email.receivers.append(zmEmail)
-    email.send("spike order review", content, files = [allSpikeOrderReview])
+#    email = Email()
+#    email.receivers.append(zmEmail)
+#    email.send("spike order review", content, files = [allSpikeOrderReview])
     
 
 # -----------------------------------------------------------------------------
