de8997267d014baabf9f7ff12da40ff8156fd0be
fcisz2502 <info@fcisz.com>
2020-04-07 16:12:42 +0800

    working on ctpWebDataComparison

--- ctpWebDataComparison.py
+++ ctpWebDataComparison.py
@@ -26,7 +26,6 @@ class CheckTradingBarsWithOtherSource(object):
     def checkTradingBars(self, instrumentList):
         for instrument in instrumentList:
             symbol = instrument[:2]
-            print("there"* 10)
             try:
                 if int(symbol[1])>=0:
                     symbol = symbol[0]
@@ -34,19 +33,18 @@ class CheckTradingBarsWithOtherSource(object):
                 pass
             
             tickSize = futureTickSize[symbol]
-            print("here"* 10)
-            self.webBars = pd.read_csv(os.path.join(self.webBars, instrument+".csv"),
+            webBars = pd.read_csv(os.path.join(self.webBars, instrument+".csv"),
                                        parse_dates=['datetime'])
             
-            self.ctpBars = pd.read_csv(os.path.join(self.ctpBarFolderDict[symbol],
+            ctpBars = pd.read_csv(os.path.join(self.ctpBarFolderDict[symbol],
                                                     self.strategyName+"_"+symbol+"_CtaTrading",
                                                     instrument+"_df_60m.csv"),
                                        parse_dates=['datetime'])
             
-            if self.ctpBars.iloc[-1]['datetime'].time() > time(22, 50):
+            if ctpBars.iloc[-1]['datetime'].time() > time(22, 50):
                 # night section
                 numberOfBars = 2     
-            elif self.ctpBars.iloc[-1]['datetime'].time() > time(14, 50):
+            elif ctpBars.iloc[-1]['datetime'].time() > time(14, 50):
                 # afternoon section
                 numberOfBars = 2
                 
@@ -54,17 +52,17 @@ class CheckTradingBarsWithOtherSource(object):
                 # three 60m bars in morning section
                 numberOfBars = 3
     #            self.webBars.reset_index(drop=True, inplace=True)
-                self.webBars.loc[len(self.webBars)-1, 'datetime'] -= timedelta(hours=0.5)
+                webBars.loc[len(webBars)-1, 'datetime'] -= timedelta(hours=0.5)
             
-            self.ctpBars = self.ctpBars.tail(numberOfBars).reset_index(drop=True)
-            self.ctpBars = self.ctpBars.loc[:, ['datetime', 'open', 'high', 'low', 'close']]
-            self.ctpBars['mergeKey'] = self.ctpBars.index
+            ctpBars = ctpBars.tail(numberOfBars).reset_index(drop=True)
+            ctpBars = ctpBars.loc[:, ['datetime', 'open', 'high', 'low', 'close']]
+            ctpBars['mergeKey'] = ctpBars.index
             
-            self.webBars = self.webBars.tail(numberOfBars).reset_index(drop=True)
-            self.webBars['mergeKey'] = self.webBars.index
-            self.webBars['datetime'] = self.webBars['datetime'] + timedelta(hours=1)
+            webBars = webBars.tail(numberOfBars).reset_index(drop=True)
+            webBars['mergeKey'] = webBars.index
+            webBars['datetime'] = webBars['datetime'] + timedelta(hours=1)
             
-            compRes = pd.merge(self.ctpBars, self.webBars, how='outer', on='mergeKey',
+            compRes = pd.merge(ctpBars, webBars, how='outer', on='mergeKey',
                                suffixes=('_ctp', '_web'))
             
             compRes['dtDiff'] = abs(compRes['datetime_ctp'] - compRes['datetime_web'])
@@ -74,7 +72,7 @@ class CheckTradingBarsWithOtherSource(object):
             compRes['lowDiff'] = compRes['low_ctp'] - compRes['low_web']
             compRes['closeDiff'] = compRes['close_ctp'] - compRes['close_web']
             
-            if sum((compRes.loc[:, 'openDiff':'closeDiff'] > tickSize).sum()) or  \
+            if sum((compRes.loc[:, 'openDiff':'closeDiff'] > tickSize).sum()) |  \
             compRes['dt>5min'].sum():
                 email = Email()
                 email.set_subjectPrefix("check-trading-bars-with-joinquant")
@@ -88,4 +86,5 @@ class CheckTradingBarsWithOtherSource(object):
 if __name__ == "__main__":
     instrumentList = ["rb2010"]
     ctb =  CheckTradingBarsWithOtherSource()
-    compRes = ctb.checkTradingBars(["MA005", "rb2010"])  # 
+    instrumentList = ['MA005', 'rb2010']
+    compRes = ctb.checkTradingBars(instrumentList)  # 
