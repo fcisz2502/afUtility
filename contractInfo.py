32df19a608ec0f8395b14f90c955e3141c8c460e
fcisz2502 <info@fcisz.com>
2020-04-07 15:53:26 +0800

    add ctpWebDataComparison

---
+++ contractInfo.py
@@ -0,0 +1,2 @@
+futureTickSize = {'bu':2, 'ru':5, 'rb':1, 'p':2, 'i':0.5, 'AP':1, 'm':1, 'RM':1, 'MA':1}
+futureLotSize = {'bu':10, 'ru' :10, 'rb':10, 'p':10, 'i':100, 'AP':10, 'm':10, 'RM':10, 'MA':10}
