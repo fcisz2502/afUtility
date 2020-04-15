939465b1c8e7c3bf0a3bf5c7ae3ce790aafb6989
fcisz2502 <info@fcisz.com>
2020-04-15 14:47:58 +0800

    add tb1mTo60 func to afUtility.py

--- afUtility.py
+++ afUtility.py
@@ -35,3 +35,42 @@ def getTbInstrumentList(instrumentList):
             tbInstrumentList.append(vtSymbol)
 
     return tbInstrumentList
+
+
+# -----------------------------------------------------------------------------
+def tb1mTo60m(instrument, lenOfDF=500):
+    tbDataPath = os.path.join(os.sep*2, "FCIDEBIAN", "FCI_Cloud", "dataProcess")
+    infile = os.path.join(tbDataPath, instrument+'(1分钟).csv')
+    if 2 == sys.version_info[0]:
+        # python2.x
+        unifile = unicode(infile , "utf8")
+    elif 3 == sys.version_info[0]:
+        # python3.x
+        import codecs
+        unifile = codecs.open(infile, 'r', encoding='utf-8')
+    else:
+        raise Exception("python version error, in afUtility.py.")
+        
+    df = pd.read_csv(unifile, 
+                     names=['datetime', 'open', 'high', 'low', 'close', 'trade', 'volume'], 
+                     parse_dates=['datetime'], 
+                     index_col = 'datetime')
+    
+    df = df.tail(700*60)
+    
+    freq = '60min'
+    df60 = df[['open']].resample(rule=freq).first()
+    df60['high'] = df[['high']].resample(rule=freq).max()
+    df60['low'] = df[['low']].resample(rule=freq).min()
+    df60['close'] = df[['close']].resample(rule=freq).last()
+    df60.dropna(inplace=True)
+    
+    df60 = df60.tail(lenOfDF)
+    
+    df60['askPrice1'] = 0
+    df60['bidPrice1'] = 0
+    df60['ft'] = 0
+    df60['symbol'] = instrument
+    df60 = df60[['symbol','open','high','low','close','askPrice1','bidPrice1','ft']]
+#    df60.to_csv(os.path.join(tbDataPath, '60_for_vnpy' , instrument+'_df_60m.csv'))
+    return df60
