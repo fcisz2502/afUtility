0738418a7b71095dd77bd3cb60d89665dc94599c
fcisz2502 <info@fcisz.com>
2020-05-14 15:20:07 +0800

    add ctaTradingOrderReviewResultGatherer.py

--- ctaTradingOrderReviewResultGatherer.py
+++ ctaTradingOrderReviewResultGatherer.py
@@ -4,6 +4,7 @@ import pandas as pd
 from afUtility.mailing import Email
 from afUtility.keyInfo import zmEmail, cwhEmail
 import copy
+from dateutil import parser
 
 
 # -----------------------------------------------------------------------------
@@ -19,6 +20,8 @@ class CtaTradingOrderReviewResultGatherer(object):
         self.subjectSuffix = None
         self.fileSaveAndAttached = None
         self.jqOrTb = str()
+        self.tradingBarsPathDict = dict()
+        self.recentOrders = None
     
     # -------------------------------------------------------------------------    
     def getAllOrderReview(self, checkList, reviewResultFolder):      
@@ -60,32 +63,61 @@ class CtaTradingOrderReviewResultGatherer(object):
         return orderReviewTotalResult
     
     # -------------------------------------------------------------------------
-    def getTodaysOrders(self):
-        # get today's orders in both real trading and backtesting
+    def getRecentOrders(self, isTbDataUsed=False, startDate=None):
+        '''get today's orders in both real trading and backtesting if joinquant data is used'''        
         if self.orderReviewTotalResult is None:
             self.getAllOrderReview()
         orderReviewTotalResult = self.orderReviewTotalResult.copy()
         
-        tradeBeginTime = datetime.combine(datetime.now().date(), time(0))
-        if 0 == datetime.today().weekday():
-            tradeBeginTime -= timedelta(hours=4+48)
-        elif datetime.today().weekday() in [1, 2, 3, 4, 5]:
-            tradeBeginTime -= timedelta(hours=4)
+        if startDate is None:
+            if isTbDataUsed:
+                try:
+                    instrument = self.tradingBarsPathDict.keys()[-1]
+                    lastBacktestingDatetime = pd.read_csv(os.path.join(self.tradingBarsPathDict[instrument], instrument+'_df_60m_ctp.csv'), 
+                                              parse_dates=['datetime'], 
+                                              index_col='datetime').index[-1]
+                    if abs(lastBacktestingDatetime - datetime.combine(lastBacktestingDatetime.date(), time(15))) < timedelta(minutes=2.5):
+                        # set time between 1500 to 2100 should be all fine.
+                        tradeBeginDatetime = datetime.combine(lastBacktestingDatetime.date(), time(16))
+                except Exception, e:
+                    raise Exception("cannot fetch last backtesting datetime in getRecentOrders() when using tradeblazer data.")
+                    self.email.send("cannot fetch last backtesting datetime in getRecentOrders() when using tradeblazer data.", repr(e))
+            else:
+                # joinquant data is used
+                tradeBeginDatetime = datetime.combine(datetime.now().date(), time(0))
+                if 0 == datetime.today().weekday():
+                    tradeBeginDatetime -= timedelta(hours=4+48)
+                elif datetime.today().weekday() in [1, 2, 3, 4, 5]:
+                    tradeBeginDatetime -= timedelta(hours=4)
+                else:
+                    tradeBeginDatetime -= timedelta(hours=4+24)
         else:
-            tradeBeginTime -= timedelta(hours=4+24)
+            if isinstance(startDate, str):
+                tradeBeginDatetime = datetime.combine(parser.parse(startDate).date(), time(0))
+            elif isinstance(startDate, datetime):
+                tradeBeginDatetime = datetime.combine(startDate.date(), time(0))
+            else:
+                raise Exception("please input startDate in str or in datetime format.")
+            if 0 == tradeBeginDatetime.weekday():
+                tradeBeginDatetime -= timedelta(hours=4+48)
+            elif tradeBeginDatetime.weekday() in [1, 2, 3, 4, 5]:
+                tradeBeginDatetime -= timedelta(hours=4)
+            else:
+                tradeBeginDatetime -= timedelta(hours=4+24)
             
         if time(11) < datetime.now().time() < time(15):
-            tradeEndTime = datetime.combine(datetime.now().date(),time(11, 30))
+            tradeEndDatetime = datetime.combine(datetime.now().date(),time(11, 30))
         elif time(15) < datetime.now().time() < time(23):
-            tradeEndTime = datetime.combine(datetime.now().date(),time(15))
+            tradeEndDatetime = datetime.combine(datetime.now().date(),time(15))
         else:
             # ??????????????????????????????????????????
             # ??????????????????????????????????????????
             # ??????????????????????????????????????????
             # what to do with night trading'''
-            tradeEndTime = datetime.combine(datetime.now().date(),time(11, 30))
-        print("tradeBeginTime", tradeBeginTime)
-        print("tradeEndTime", tradeEndTime)
+            pass
+#            tradeEndDatetime = datetime.combine(datetime.now().date(), time(11, 30))
+        print("tradeBeginDatetime", tradeBeginDatetime)
+        print("tradeEndDatetime", tradeEndDatetime)
         
         if len(orderReviewTotalResult):
             orderReviewTotalResult['rt_last_enter_datetime'].fillna(datetime(2019,1,1), inplace=True)
@@ -93,24 +125,25 @@ class CtaTradingOrderReviewResultGatherer(object):
             orderReviewTotalResult['bt_last_enter_datetime'].fillna(datetime(2019,1,1), inplace=True)
             orderReviewTotalResult['bt_last_depart_datetime'].fillna(datetime(2019,1,1), inplace=True)
             
-            self.todaysOrder = orderReviewTotalResult.loc[(orderReviewTotalResult['rt_last_enter_datetime']>tradeBeginTime) | (
-                                                    (tradeBeginTime < orderReviewTotalResult['rt_last_depart_datetime']) & (
-                                                            orderReviewTotalResult['rt_last_depart_datetime'] < tradeEndTime)) | (
-                                                    orderReviewTotalResult['bt_last_enter_datetime']>tradeBeginTime) | (
-                                                    (tradeBeginTime < orderReviewTotalResult['bt_last_depart_datetime']) & (
-                                                            orderReviewTotalResult['bt_last_depart_datetime'] < tradeEndTime))]
+            self.recentOrders = orderReviewTotalResult.loc[(orderReviewTotalResult['rt_last_enter_datetime']>tradeBeginDatetime) | (
+                                                    (tradeBeginDatetime < orderReviewTotalResult['rt_last_depart_datetime']) & (
+                                                            orderReviewTotalResult['rt_last_depart_datetime'] < tradeEndDatetime)) | (
+                                                    orderReviewTotalResult['bt_last_enter_datetime']>tradeBeginDatetime) | (
+                                                    (tradeBeginDatetime < orderReviewTotalResult['bt_last_depart_datetime']) & (
+                                                            orderReviewTotalResult['bt_last_depart_datetime'] < tradeEndDatetime))]
         else:
-            self.todaysOrder = pd.DataFrame()
+            self.recentOrders = pd.DataFrame()
     
     # -------------------------------------------------------------------------    
-    def rearangeTodysOrders(self):
-        self.getTodaysOrders()
+    def rearangeRecentOrders(self):
+        if self.recentOrders is None:
+            self.getRecentOrders()
         # rearange today's order(s) before sending them 
         pairComp = pd.DataFrame(columns = ['instrument', 'orderNumber', 'openDatetime', 'closeDatetime', 
                                            'openPrice', 'closePrice', 'volume', 'direction', 'sl', 'tp',
                                            "volFac", "range", "balanceAtOrder"])
         todayOrderPresented = pd.DataFrame(columns=['column', 'realTrading', 'backtesting'])
-        for row in self.todaysOrder.iterrows():
+        for row in self.recentOrders.iterrows():
             pairComp.loc[len(pairComp), :] = [row[1]['strategyID']] + list(row[1]['rt_index':'rt_balanceAtOrder'])
             pairComp.loc[len(pairComp), :] = [row[1]['strategyID']] + list(row[1]['bt_index':'bt_balanceAtOrder'])
             tempDF = pairComp.T
@@ -133,12 +166,16 @@ class CtaTradingOrderReviewResultGatherer(object):
         return todayOrderPresented
     
     # -------------------------------------------------------------------------
-    def sendTodaysOrders(self):
-        todaysOrderPresented_ = self.rearangeTodysOrders()
+    def sendRecentOrders(self):
+        recentOrderPresented_ = self.rearangeRecentOrders()
         self.email.set_subjectPrefix(self.jqOrTb)
-        self.email.send("Review "+self.strategyName+"'s order(s) today: ", 
-                   todaysOrderPresented_.to_html(justify='left'),
-                   files = [self.fileSaveAndAttached])
+        if 'tb' in self.jqOrTb:
+            subject = "recently"
+        else:
+            subject = "today"
+        self.email.send("Review %s's order(s) %s." % (self.strategyName, subject), 
+                        recentOrderPresented_.to_html(justify='left'),
+                        files = [self.fileSaveAndAttached])
     
     
 # -----------------------------------------------------------------------------
@@ -149,19 +186,21 @@ if __name__ == "__main__":
     # jointquant data bt result
     reviewResultFolder = os.path.join(os.sep*2, "FCIDEBIAN", "FCI_Cloud", 
                                       "dataProcess", "future_daily_data", 
-                                      strategyName[0].lower()+strategyName[1:]+"OrderReview")
+                                      strategyName.lower()+"OrderReview")
     # tb data bt result
     reviewResultFolderTb = os.path.join(os.sep*2, "FCIDEBIAN", "FCI_Cloud", "dataProcess", 
                                         "future_daily_data", "reviewWithTbData",
-                                        strategyName[0].lower()+strategyName[1:]+"OrderReview")
+                                        strategyName.lower()+"OrderReview")
     
     for path in [reviewResultFolder, reviewResultFolderTb]: 
         gatherer = CtaTradingOrderReviewResultGatherer(strategyName)
+        gatherer.getAllOrderReview(copy.deepcopy(checkList), path)
+        # gatherer.email.receivers.append(zmEmail)
+        gatherer.email.set_receivers([cwhEmail])
         if "TbData" in path:
             gatherer.jqOrTb = "tbData"
+            gatherer.getRecentOrders(startDate='20200513')
         else:
             gatherer.jqOrTb = "jqData"
-#        gatherer.email.receivers = [cwhEmail]
-    #        gatherer.email.receivers.append(zmEmail)
-        gatherer.getAllOrderReview(copy.deepcopy(checkList), path)
-        gatherer.sendTodaysOrders()
+            gatherer.getRecentOrders(isTbDataUsed=True, startDate='20200513')
+        gatherer.sendRecentOrders()
