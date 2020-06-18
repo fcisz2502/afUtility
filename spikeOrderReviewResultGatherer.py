681db51e484972bed0a8df382fb9f7b99d164710
fcisz2502 <info@fcisz.com>
2020-06-18 16:42:10 +0800

    update spikeOrderReviewResultGatherer.py

--- spikeOrderReviewResultGatherer.py
+++ spikeOrderReviewResultGatherer.py
@@ -3,7 +3,6 @@ from datetime import datetime, time
 import pandas as pd
 from afUtility.mailing import Email
 from afUtility.keyInfo import zmEmail, cwhEmail
-import copy
 
 
 # -----------------------------------------------------------------------------
@@ -14,21 +13,22 @@ def getAllOrderReview():
     '''
 #    reviewResultFolder = "\\\\FCIDEBIAN\\FCI_Cloud\\dataProcess\\spike stocks\\orderReview"
     reviewResultFolder = os.path.join(os.sep*2, "FCIDEBIAN", "FCI_Cloud", "dataProcess", "spike stocks", "orderReview")
-    checkList = ["VM2CF_spike2", "VM2CF_spikeOLOS", "VM4YYC_spike2", "VM5QS_spike2"]
+    checkList = ["VM2CF_spike2", "VM2CF_spikeOLOS", "VM4YYC_spike2", 
+                 "VM4YYC_spikeOLOS", "VM5QS_spike2", "VM5QS_spikeOLOS"]
     today = str(datetime.today().date())
     
     orderReviewTotalResult = pd.DataFrame()
     for doc in os.listdir(reviewResultFolder):
         if today in doc.split("_") and (not "allSpikeOrderReview.csv" in doc.split("_")):
-            checkList.remove(doc[18:-4])
-            orderReviewTotalResult = pd.concat([orderReviewTotalResult, pd.read_csv(os.path.join(reviewResultFolder, doc))], sort=False)
-#            except TypeError:
-#                orderReviewTotalResult = pd.concat([orderReviewTotalResult, pd.read_csv(os.path.join(reviewResultFolder, doc))])
+            try:
+                checkList.remove(doc[18:-4])
+                orderReviewTotalResult = pd.concat([orderReviewTotalResult, pd.read_csv(os.path.join(reviewResultFolder, doc))], sort=False)
+            except ValueError:
+                print("doc is %s, which may not be included in the checkList." % doc)
     if len(checkList):
-        subjectSuffix = "Order review result(s) from %s have not been found!"%checkList
+        subjectSuffix = "Order review result(s) from %s have not been found!" % checkList
     else:
-        subjectSuffix = "Order review for %s have been done!"%["VM2_spike2", "VM2_spikeOLOS", "VM4_spike2", "VM5_spike2"]
-    
+        subjectSuffix = "Order review for %s have been done!" % checkList
     fileSaveAndAttached = os.path.join(reviewResultFolder, datetime.now().strftime("%Y-%m-%d_%H%M%S")+"_allSpikeOrderReview.csv")
     orderReviewTotalResult.to_csv(fileSaveAndAttached, index=0)
     
