f05d7ab2184d247e456fe0f44adb6eae5651521e
fcisz2502 <info@fcisz.com>
2020-08-31 17:23:09 +0800

    update gatherer

--- ctaTradingOrderReviewResultGatherer.py
+++ ctaTradingOrderReviewResultGatherer.py
@@ -39,36 +39,41 @@ class CtaTradingOrderReviewResultGatherer(object):
     def getAllOrderReview(self):
         self.checkJqOrTbDataUsed()
             
-        checkListCopy = self.checkList[:]
+        checkListCopy = [item.lower() for item in self.checkList]
         today = str(datetime.today().date())
         
         orderReviewTotalResult = pd.DataFrame()
         for doc in os.listdir(self.reviewResultFolder):
             # look at last night's order(s)
             if datetime.now().time() < time(9):
-                if today in doc.split("_") and (int(doc[11:17])<90000) and (not "all"+self.strategyName+"OrderReview.csv" in doc.split("_")):
-                    checkListCopy.remove(doc[18:-4])
+                if today in doc.split("_") and (parser.parse(' '.join([doc.split('_')[0], doc.split('_')[1]])).time() < time(9)) \
+                    and (not "all"+self.strategyName+"OrderReview.csv" in doc.split("_")):
+                    # doc[18:-4] =>'machineID_strategy_xx', xx=> 'jq' or 'tb'
+                    checkListCopy.remove(doc[18:-4].lower())
                     orderReviewTotalResult = pd.concat([orderReviewTotalResult, pd.read_csv(os.path.join(self.reviewResultFolder, doc))], sort=False)
             # moring section order(s)
             elif datetime.now().time() < time(15):
-                if today in doc.split("_") and (90000<int(doc[11:17])<150000) and (not "all"+self.strategyName+"OrderReview.csv" in doc.split("_")):
-                    checkListCopy.remove(doc[18:-4])
+                if today in doc.split("_") and (time(9) < parser.parse(' '.join([doc.split('_')[0], doc.split('_')[1]])).time() < time(15)) \
+                    and (not "all"+self.strategyName+"OrderReview.csv" in doc.split("_")):
+                    checkListCopy.remove(doc[18:-4].lower())
                     orderReviewTotalResult = pd.concat([orderReviewTotalResult, pd.read_csv(os.path.join(self.reviewResultFolder, doc))], sort=False)
             # afternoon section order(s)
             else:
-                if today in doc.split("_") and (int(doc[11:17])>150000) and (not "all"+self.strategyName+"OrderReview.csv" in doc.split("_")):
-                    checkListCopy.remove(doc[18:30])
+                if today in doc.split("_") and (parser.parse(' '.join([doc.split('_')[0], doc.split('_')[1]])).time() > time(15)) \
+                    and (not "all"+self.strategyName+"OrderReview.csv" in doc.split("_")):
+                    # doc[18:-7] => 'machineID_strategy'
+                    checkListCopy.remove(doc[18:-7].lower())
                     orderReviewTotalResult = pd.concat([orderReviewTotalResult, pd.read_csv(os.path.join(self.reviewResultFolder, doc))], sort=False)
         if len(checkListCopy):
-            self.subjectSuffix = "Order review result(s) from %s have not been found!" % checkListCopy
+            self.subjectSuffix = "Order review result(s) from %s have not been found!" %checkListCopy
         else:
-            self.subjectSuffix = "Order review for %s have been done!" % self.checkList
+            self.subjectSuffix = "Order review for %s have been done!" %self.checkList
         
         if len(orderReviewTotalResult):
-            orderReviewTotalResult['rt_last_enter_datetime'] = pd.to_datetime(orderReviewTotalResult['rt_last_enter_datetime'])
-            orderReviewTotalResult['rt_last_depart_datetime'] = pd.to_datetime(orderReviewTotalResult['rt_last_depart_datetime'])
-            orderReviewTotalResult['bt_last_enter_datetime'] = pd.to_datetime(orderReviewTotalResult['bt_last_enter_datetime'])
-            orderReviewTotalResult['bt_last_depart_datetime'] = pd.to_datetime(orderReviewTotalResult['bt_last_depart_datetime'])
+            orderReviewTotalResult['rt_openDatetime'] = pd.to_datetime(orderReviewTotalResult['rt_openDatetime'])
+            orderReviewTotalResult['rt_closeDatetime'] = pd.to_datetime(orderReviewTotalResult['rt_closeDatetime'])
+            orderReviewTotalResult['bt_openDatetime'] = pd.to_datetime(orderReviewTotalResult['bt_openDatetime'])
+            orderReviewTotalResult['bt_closeDatetime'] = pd.to_datetime(orderReviewTotalResult['bt_closeDatetime'])
         
         self.fileSaveAndAttached = os.path.join(self.reviewResultFolder, datetime.now().strftime("%Y-%m-%d_%H%M%S")+"_all"+self.strategyName+"OrderReview.csv")
         orderReviewTotalResult.to_csv(self.fileSaveAndAttached, index=0)
@@ -80,7 +85,7 @@ class CtaTradingOrderReviewResultGatherer(object):
     # -------------------------------------------------------------------------
     def getRecentOrders(self, startDate=None):
         '''
-        Det today's orders in both real trading and backtesting if joinquant data is used.
+        Get today's orders in both real trading and backtesting if joinquant data is used.
         If startDate is None, today's date will be used for joinquant data, or the date after 
         last order review date for tradeblazer data will be used.
         Turn isTbDataUsed to False if use joinquant data, to True for Tb data.
@@ -139,22 +144,18 @@ class CtaTradingOrderReviewResultGatherer(object):
             # ??????????????????????????????????????????
             # what to do with night trading'''
             pass
-#            tradeEndDatetime = datetime.combine(datetime.now().date(), time(11, 30))
-        print("tradeBeginDatetime", tradeBeginDatetime)
-        print("tradeEndDatetime", tradeEndDatetime)
-        
         if len(orderReviewTotalResult):
-            orderReviewTotalResult['rt_last_enter_datetime'].fillna(datetime(2019,1,1), inplace=True)
-            orderReviewTotalResult['rt_last_depart_datetime'].fillna(datetime(2019,1,1), inplace=True)
-            orderReviewTotalResult['bt_last_enter_datetime'].fillna(datetime(2019,1,1), inplace=True)
-            orderReviewTotalResult['bt_last_depart_datetime'].fillna(datetime(2019,1,1), inplace=True)
+            orderReviewTotalResult['rt_openDatetime'].fillna(datetime(2019,1,1), inplace=True)
+            orderReviewTotalResult['rt_closeDatetime'].fillna(datetime(2019,1,1), inplace=True)
+            orderReviewTotalResult['bt_openDatetime'].fillna(datetime(2019,1,1), inplace=True)
+            orderReviewTotalResult['bt_closeDatetime'].fillna(datetime(2019,1,1), inplace=True)
             
-            self.recentOrders = orderReviewTotalResult.loc[(orderReviewTotalResult['rt_last_enter_datetime']>tradeBeginDatetime) | (
-                                                    (tradeBeginDatetime < orderReviewTotalResult['rt_last_depart_datetime']) & (
-                                                            orderReviewTotalResult['rt_last_depart_datetime'] < tradeEndDatetime)) | (
-                                                    orderReviewTotalResult['bt_last_enter_datetime']>tradeBeginDatetime) | (
-                                                    (tradeBeginDatetime < orderReviewTotalResult['bt_last_depart_datetime']) & (
-                                                            orderReviewTotalResult['bt_last_depart_datetime'] < tradeEndDatetime))]
+            self.recentOrders = orderReviewTotalResult.loc[(orderReviewTotalResult['rt_openDatetime']>tradeBeginDatetime) | (
+                                                    (tradeBeginDatetime < orderReviewTotalResult['rt_closeDatetime']) & (
+                                                            orderReviewTotalResult['rt_closeDatetime'] < tradeEndDatetime)) | (
+                                                    orderReviewTotalResult['bt_openDatetime']>tradeBeginDatetime) | (
+                                                    (tradeBeginDatetime < orderReviewTotalResult['bt_closeDatetime']) & (
+                                                            orderReviewTotalResult['bt_closeDatetime'] < tradeEndDatetime))]
         else:
             self.recentOrders = pd.DataFrame()
     
@@ -162,14 +163,16 @@ class CtaTradingOrderReviewResultGatherer(object):
     def rearangeRecentOrders(self):
         if self.recentOrders is None:
             self.getRecentOrders()
+        print('recentOrders.column ', self.recentOrders.columns)
         # rearange today's order(s) before sending them 
-        pairComp = pd.DataFrame(columns = ['instrument', 'orderNumber', 'openDatetime', 'closeDatetime', 
-                                           'openPrice', 'closePrice', 'volume', 'direction', 'sl', 'tp',
-                                           "volFac", "range", "balanceAtOrder"])
+        columnList = ['instrument', 'orderNumber', 'openDatetime', 'closeDatetime', 
+                      'openPrice', 'closePrice', 'strategy', 'volume', 'direction', 
+                      'sl', 'tp', 'volFac', 'range', 'balanceAtOrder', 'csrh', 'csrl']
         todayOrderPresented = pd.DataFrame(columns=['column', 'realTrading', 'backtesting'])
+        pairComp = pd.DataFrame(columns = columnList)
         for row in self.recentOrders.iterrows():
-            pairComp.loc[len(pairComp), :] = [row[1]['strategyID']] + list(row[1]['rt_index':'rt_balanceAtOrder'])
-            pairComp.loc[len(pairComp), :] = [row[1]['strategyID']] + list(row[1]['bt_index':'bt_balanceAtOrder'])
+            pairComp.loc[len(pairComp), :] = [row[1]['strategyID']] + list(row[1]['rt_index':'rt_csrl'])
+            pairComp.loc[len(pairComp), :] = [row[1]['strategyID']] + list(row[1]['bt_index':'bt_csrl'])
             tempDF = pairComp.T
             tempDF.reset_index(inplace=True)
             tempDF.rename(columns={"index": "column", 0: "realTrading", 1: "backtesting"}, inplace=True)
@@ -177,9 +180,7 @@ class CtaTradingOrderReviewResultGatherer(object):
             todayOrderPresented.reset_index(drop=True, inplace=True)
             tempDF = None
             todayOrderPresented.loc[todayOrderPresented.shape[0], 'column': "backtesting"]= [str(), str(), str()]
-            pairComp = pd.DataFrame(columns = ['stock', 'orderNumber', 'openDatetime', 'closeDatetime', 
-                                               'openPrice', 'closePrice', 'volume', 'direction', 'sl', 'tp',
-                                               "volFac", "range", "balanceAtOrder"])
+            pairComp = pd.DataFrame(columns = columnList)
     
         todayOrderPresented.set_index('column', inplace=True, drop=True)
         
@@ -206,15 +207,15 @@ class CtaTradingOrderReviewResultGatherer(object):
     
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
-    strategyName = "Probot"
-    probotCheckList = ["VM2CF_probot"]
+    strategyName = "csrProbot"
+    strategyCheckList = ["VM2CF_csrProbot"]
     
-    probotTradingBarFolderDict = {
-        "rb2010": os.path.join("C:", os.sep, "vnpy-1.9.2", "examples", "ctaTrading",
-                               "paperTrading", "probot_rb"),
-        "MA009": os.path.join("C:", os.sep, "vnpy-1.9.2", "examples", "ctaTrading",
-                              "paperTrading", "probot_MA")
-    }
+#    csrProbotTradingBarFolderDict = {
+#        "rb2010": os.path.join("C:", os.sep, "vnpy-1.9.2", "examples", "ctaTrading",
+#                               "paperTrading", "probot_rb"),
+#        "MA009": os.path.join("C:", os.sep, "vnpy-1.9.2", "examples", "ctaTrading",
+#                              "paperTrading", "probot_MA")
+#    }
     
     # jointquant data bt result
     reviewResultFolder = os.path.join(os.sep*2, "FCIDEBIAN", "FCI_Cloud", 
@@ -229,7 +230,7 @@ if __name__ == "__main__":
 #    gatherer.email.receivers.append(zmEmail)
     gatherer.email.set_receivers([cwhEmail])
     
-    gatherer.checkList= probotCheckList[:]
+    gatherer.checkList= strategyCheckList[:]
     
     '''jq data'''
     gatherer.reviewResultFolder = reviewResultFolder
@@ -239,5 +240,5 @@ if __name__ == "__main__":
     Run this sector on according mathine so that it will 
     get the tradingBarsFolderDict correctly'''
 #    gatherer.reviewResultFolder = reviewResultFolderTb
-#    gatherer.tradingBarsPathDict = probotTradingBarFolderDict
+#    gatherer.tradingBarsPathDict = csrProbotTradingBarFolderDict
 #    gatherer.sendRecentOrders(startDate=None)
